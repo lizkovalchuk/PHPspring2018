0749cbdb5500aa0a02f3d98085e2d9ba95f7a3d5
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-03-05 14:41:36 -0500

    delete file

--- lab4/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <section id="social-media">
-
-    </section>
-    <p>©Make Me Mozart Inc, 2018. All rights reserved.</p>
-</footer>
--- lab4/header.php
+++
@@ -1,3 +0,0 @@
-<h1 ID="hidden">Make Me Mozart</h1>
-<img id='logo' src='images/logo.jpg'>
-<p>Contact us: admin@makememozart.com</p>
